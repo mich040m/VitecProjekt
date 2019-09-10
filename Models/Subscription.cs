8c3683dab0e2dad6231103e5e9e5df4fbdd055b6
Michal Tomasz Odzienkowski <mich040m@edu.ucl.dk>
2019-09-10 11:58:47 +0200

    folder

---
+++ Models/Subscription.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecProjekt.Models
+{
+    public class Subscription
+    {
+    }
+}
