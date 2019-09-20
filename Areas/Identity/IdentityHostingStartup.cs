45e0c7b44a053dee6a00f2d3143c651ece78ef0c
Michal Tomasz Odzienkowski <mich040m@edu.ucl.dk>
2019-09-20 11:48:00 +0200

    Login system made using Micosoft Identity template  and EntityFramework

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VitecProjekt.Areas.Identity.Data;
+using VitecProjekt.Models;
+
+[assembly: HostingStartup(typeof(VitecProjekt.Areas.Identity.IdentityHostingStartup))]
+namespace VitecProjekt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VitecProjektContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VitecProjektContextConnection")));
+
+                services.AddDefaultIdentity<VitecUser>()
+                    .AddEntityFrameworkStores<VitecProjektContext>();
+            });
+        }
+    }
+}
