50433dcdcb05bdefa91b7d695f72386a4a6b947f
Peter Starup <pete59f1@edu.ucl.dk>
2019-09-27 11:56:31 +0200

    Har lige tilføjet lidt kommentar

--- Areas/Identity/Data/VitecUser.cs
+++ Areas/Identity/Data/VitecUser.cs
@@ -10,6 +10,7 @@ namespace VitecProjekt.Areas.Identity.Data
     // Add profile data for application users by adding properties to the VitecUser class
     public class VitecUser : IdentityUser
     {
+        //The Timestamp attribute specifies that this column will be included in the Where clause of Update and Delete commands sent to the database
         [Timestamp]
         public byte[] RowVersion { get; set; }
     }
