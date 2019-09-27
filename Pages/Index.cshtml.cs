32df4fac16435ab5ffb52ce06b82d79b6b9bad77
Michal Tomasz Odzienkowski <mich040m@edu.ucl.dk>
2019-09-27 12:08:02 +0200

    Changing connection string to our Azure DB

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -19,6 +19,7 @@ namespace VitecProjekt.Pages
         {
             
             telemetry.TrackEvent("Main page loaded");
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ namespace VitecProjekt
                 logging.ClearProviders();
                 logging.AddConsole();
                 logging.AddDebug();
+                
                 });
     }
 }
