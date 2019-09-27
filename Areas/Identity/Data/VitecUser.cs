c304419efaca732076d0d5ea4abb9a5396cbd797
Peter Starup <pete59f1@edu.ucl.dk>
2019-09-27 11:53:14 +0200

    Har tilføjet en Timestamp til vores User til at hjælpe med concurrency conflits. Vi kan ikke tjekke om det virker, da vi ikke har nogen edit, delete osv til user

--- Areas/Identity/Data/VitecUser.cs
+++ Areas/Identity/Data/VitecUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,7 @@ namespace VitecProjekt.Areas.Identity.Data
     // Add profile data for application users by adding properties to the VitecUser class
     public class VitecUser : IdentityUser
     {
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
     }
 }
