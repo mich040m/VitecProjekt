2fbb8090c228d0258f146e0a613e136f49704d47
Peter Starup <pete59f1@edu.ucl.dk>
2019-12-03 10:29:26 +0100

    Har lavet en seed klasse som laver en role Admin og en user som har den role

---
+++ Data/SeedDB.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecProjekt.Data
+{
+    public static class SeedDB
+    {
+        public static async Task Seed(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string[] roleNames = { "Admin" };
+            IdentityResult roleResult;
+
+            //Tilføjer roles til DB
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1  
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Tilføjer en admin user til DB
+            IdentityUser user = await userManager.FindByEmailAsync("Admin@gmail.com");
+
+            if (user == null)
+            {
+                user = new IdentityUser()
+                {
+                    UserName = "Admin@gmail.com",
+                    Email = "Admin@gmail.com",
+                };
+                await userManager.CreateAsync(user, "Admin!123");
+            }
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.ApplicationInsights;
 using Microsoft.EntityFrameworkCore;
 using VitecProjekt.Models;
+using VitecProjekt.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 
 namespace VitecProjekt
 {
@@ -48,18 +51,17 @@ namespace VitecProjekt
             });
 
             services.AddApplicationInsightsTelemetry();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<VitecProjektContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("VitecProjektContext")));
 
             services.AddDbContext<VitecProjektModelsContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("VitecProjektModelsContext")));
-            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             
             if (env.IsDevelopment())
@@ -85,6 +87,8 @@ namespace VitecProjekt
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //SeedDB.Seed(serviceProvider).Wait();
         }
     }
 }
